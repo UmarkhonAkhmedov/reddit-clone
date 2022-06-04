70430a5887aa522238d482d4cc913af4ac772493
Umarkhon <umarkhonakhmedov2000@gmail.com>
2022-06-05 01:34:02 +0500

    Changing files, deleting files and recreating files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
-import Header from '../components/Header'
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <div className='h-screen overflow-y-scroll bg-slate-200'>
-        <Header />
-        <Component {...pageProps} />
-      </div>
-      
-    </SessionProvider>
-  )
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Header from '../components/Header'
+
 
 const Home: NextPage = () => {
   return (
     <div className="">
       <Head>
-        <title>Reddit 2.0 Clone</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Reddit Clone</title>
       </Head>
-
+      
+      <Header />
+      
     </div>
   )
 }
